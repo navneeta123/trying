cd8489ec8b1e6e76b40f460c31c938600c76c1cb
navneeta123 <43060987+navneeta123@users.noreply.github.com>
2019-05-12 00:59:33 +0800

    changed test.php

--- test.php
+++ test.php
@@ -1,8 +1,3 @@
 <?php
-$serialized_data = serialize(array('Math', 'Language', 'Science'));
-echo  $serialized_data . '<br>';
-// Unserialize the data
-$var1 = unserialize($serialized_data);
-// Show the unserialized data;
-var_dump ($var1);
+echo "hello world";
 ?>
